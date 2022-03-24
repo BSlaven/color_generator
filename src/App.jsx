f62d9e2723aeebc5978074c89a055dc3eda428cf
Slaven B <slaven.bunijevac@gmail.com>
2022-03-24 22:27:03 +0100

    add error text into the structure

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,21 @@ import ColorCard from './ColorCard/ColorCard';
 function App() {
   const [ color, setColor ] = useState('#00FF70');
   const [ allColors, setAllColors ] = useState([]);
-  const [ error, setError ] = useState(false);
+  const [ error, setError ] = useState(true);
 
   useEffect(() => {
     generateArrayOfColors(color);
   }, [color]);
 
   const generateArrayOfColors = base => {
-    const result = new Values(base);
-    const colors = result.all(10).map(item => item.hex);
-    setAllColors(colors);
+    try {
+      const result = new Values(base);
+      const colors = result.all(10).map(item => item.hex);
+      setAllColors(colors);
+      setError(true)
+    } catch {
+      setError(true);
+    }
   }
 
   const changeColor = newColor => {
@@ -26,6 +31,7 @@ function App() {
   return (
     <div className="App">
       <ColorForm changeColor={changeColor} />
+      {error ? <span className='error'>Incorrect color value. Try again.</span> : null}
       <section className="section">
         <h3 className='title'>Your colors</h3>
         {allColors && allColors.map(item => <ColorCard color={item} key={item} /> )}
