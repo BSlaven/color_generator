cccddc1fdad2218d9a617e163dfa66789f4574e0
Slaven B <slaven.bunijevac@gmail.com>
2022-03-24 22:52:15 +0100

    ad addToClipboard function to a card

--- src/ColorCard/ColorCard.jsx
+++ src/ColorCard/ColorCard.jsx
@@ -1,14 +1,19 @@
 import classes from './ColorCard.module.css';
 
 const ColorCard = ({ color }) => {
+
+  const copyToClipboard = e => {
+    navigator.clipboard.writeText(color);
+  }
+  
   return (
-    <>
+    <div onClick={copyToClipboard}>
       <p className={classes.colorParagraph}>{`#${color}`}</p>
       <div 
         style={{backgroundColor: `#${color}`}}
         className={classes.colorContainer}>
       </div>
-    </>
+    </div>
   )
 }
 
